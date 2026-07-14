f64222c93f31e76a8599dc094909d60d8c73f56c
vasu <vasudhruv1920@gmail.com>
2026-07-14 12:56:38 +0530

    Bump fused-render to 0.2.2

--- Casks/fused-render.rb
+++ Casks/fused-render.rb
@@ -1,6 +1,6 @@
 cask "fused-render" do
-  version "0.2.1"
-  sha256 "a851675e5bac31b17f9aed93bbb0822b44ced969e3c9246775a1df02bbb9398c"
+  version "0.2.2"
+  sha256 "5a17c1343ed7c3dbb389a8743f4b38f5fbecf9537f9c1323a705febe0858c1a1"
 
   url "https://fused-magic.s3.us-west-2.amazonaws.com/fused-render-dmgs/FusedRender-#{version}.dmg",
       verified: "fused-magic.s3.us-west-2.amazonaws.com/fused-render-dmgs/"
