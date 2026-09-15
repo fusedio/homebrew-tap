48dfe2dfdf6ac50bf60d411ae65ee2787cd60570
fused-render-app-ci <ci@fused.io>
2026-09-15 15:22:24 +0000

    render-app 0.8.8

--- Casks/render-app.rb
+++ Casks/render-app.rb
@@ -1,6 +1,6 @@
 cask "render-app" do
-  version "0.8.7"
-  sha256 "1518f005443c1f624c196b81353588c156ddbf10b78f6cad2fb365eb022aa6a4"
+  version "0.8.8"
+  sha256 "6e3650600270f0bf0f77666802d6b3d78d6bbc68e4ce7a8d1b8e8c510eca7583"
 
   url "https://github.com/fusedio/fused-render-lite/releases/download/v#{version}/RenderApp-#{version}.dmg"
   name "Render App"
