1b12465613efa4ee6548200d62638a3c30a97424
fused-render-lite-ci <ci@fused.io>
2026-09-15 13:27:46 +0000

    fused-render-lite 0.8.7

--- Casks/fused-render-lite.rb
+++ Casks/fused-render-lite.rb
@@ -1,6 +1,6 @@
 cask "fused-render-lite" do
-  version "0.8.6"
-  sha256 "cbe846c8181a32cb04edee3fdd28553f817df2e6c9683dff9264730f1173bbeb"
+  version "0.8.7"
+  sha256 "1518f005443c1f624c196b81353588c156ddbf10b78f6cad2fb365eb022aa6a4"
 
   url "https://github.com/fusedio/fused-render-lite/releases/download/v#{version}/RenderLite-#{version}.dmg"
   name "Fused Render Lite"
