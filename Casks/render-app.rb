7b5b465ff61ba4c0623a0bef5ca69dec503e65a2
fused-render-app-ci <ci@fused.io>
2026-09-17 17:39:07 +0000

    render-app 0.9.0

--- Casks/render-app.rb
+++ Casks/render-app.rb
@@ -1,6 +1,6 @@
 cask "render-app" do
-  version "0.8.16"
-  sha256 "1bbe756854a2357b52c81039caf0735db0fc8cd6f740052298a1b440b1dbf524"
+  version "0.9.0"
+  sha256 "3dfbe5d6d913087bee04a0a96ee8f3b703bbce1a32e77b96a49f5c774599d804"
 
   url "https://d2ic19jpchjovp.cloudfront.net/render-app-dmgs/RenderApp-#{version}.dmg",
       verified: "d2ic19jpchjovp.cloudfront.net/render-app-dmgs/"
