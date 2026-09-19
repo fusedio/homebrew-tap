42a3653361cce38f2b2741835a4149e3ccae724a
fused-render-ci <ci@fused.io>
2026-09-19 18:36:40 +0000

    fused-render 0.5.65

--- Casks/fused-render.rb
+++ Casks/fused-render.rb
@@ -1,6 +1,6 @@
 cask "fused-render" do
-  version "0.5.64"
-  sha256 "d426598b5ccc50a6ed225a7fb6394554b6049ea71b4f68dfb3d69c6b545a547d"
+  version "0.5.65"
+  sha256 "e84392e44fb3390bc0f877ce4ebb041403f81a9ba0e363fd418c01689343d2be"
 
   url "https://d2ic19jpchjovp.cloudfront.net/fused-render-dmgs/FusedRender-#{version}.dmg",
       verified: "d2ic19jpchjovp.cloudfront.net/fused-render-dmgs/"
